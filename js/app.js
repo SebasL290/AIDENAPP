718d6035001b09b548add418ef611ccfd897024b
stefwebsite <hurtstefy@gmail.com>
2025-04-11 15:26:05 -0500

    cambio js

--- js/app.js
+++ js/app.js
@@ -24,10 +24,10 @@ let mountains_front = document.getElementById('mountains_front');
 
     window.addEventListener('scroll', function (){
         let value = window.scrollY;
-        stars.style.left = value * 0 + 'px' ;
-        avion.style.left = value * 0.35 + 'px' ;
-        moon.style.top = value * 0.45 + 'px' ;
-        mountains_behind.style.top = value * 0.2 + 'px' ;
+        stars.style.top = value * 0.25 + 'px' ;
+        avion.style.left = value * 0.45 + 'px' ;
+        moon.style.top = value * 0.50 + 'px' ;
+        mountains_behind.style.top = value * 0.7 + 'px' ;
         mountains_front.style.left = value * 0 + 'px' ;
     })
 
